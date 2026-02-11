9ee392665bc903643438036cdebcc5848293719b
choosepure <support@choosepure.in>
2026-02-11 22:28:58 +0530

    Configure server to serve logo and favicon from public folder

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Email transporter configuration
 const transporter = nodemailer.createTransport({
